7cc2ce12382dc92a3613e9885d99435560b1ef24
Abdul Murtazaliev <Abdul_murtazaliyev@bk.ru>
2024-01-26 23:40:48 +0300

    commit end calculator

--- js/script.js
+++ js/script.js
@@ -4,27 +4,26 @@ document.addEventListener('DOMContentLoaded', function(){
     form.addEventListener('submit', function(e){
         e.preventDefault()
     })
-    var res_field = document.querySelector(".calculator_result-field")
+    var res_field = document.querySelector(".calculator__result-field")
 
-    var btn_num = document.querySelector(".num")
-    //let btn_reset = document.querySelectorAll(".js--btn--reset")
+    var btn_num = document.querySelectorAll(".btn-num")
+    let btn_reset = document.querySelector(".btn-reset")
     var btn_calc = document.querySelector(".sign-enter")
     for(i=0; i < btn_num.length; i++){
         btn_num[i].addEventListener("click", function(e){
             e.preventDefault()
             res_field.value += this.innerHTML;
-
         })
     }
-    // btn_reset.addEventListener("click", function(e){
-    //     e.preventDefault()
-    //     res_field.value += '';
+    btn_reset.addEventListener('click', function(e){
+        e.preventDefault()
+        res_field.value = '';
 
-    // })
+    })
     btn_calc.addEventListener('click', function(e){
         e.preventDefault()
-        res_field.value += eval(res_field.value);
+        res_field.value += (` = ${eval(res_field.value)}`);
 
-    });
-
-})
+    })
+    
+});
